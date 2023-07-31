f7e4485c27ac5cd4a8ad48924f25471f4966a4c6
pola <bolapop40@gmail.com>
2023-07-31 14:08:55 -0700

    c a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,16 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j;
-
-    for (i = 0; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            _putchar(argv[i][j]);
-        }
-        _putchar('\n');
-    }
-
-    return (0);
+int i, j;
+for (i = 0; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+_putchar(argv[i][j]);
+}
+_putchar('\n');
+}
+return (0);
 }
