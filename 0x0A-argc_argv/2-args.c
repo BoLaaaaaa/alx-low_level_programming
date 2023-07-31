597650265c2e9af991a2c8fee850ab392e06b76f
pola <bolapop40@gmail.com>
2023-07-31 14:06:03 -0700

    c a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    int i, j;
+
+    for (i = 0; i < argc; i++)
+    {
+        for (j = 0; argv[i][j]; j++)
+        {
+            _putchar(argv[i][j]);
+        }
+        _putchar('\n');
+    }
+
+    return (0);
+}
