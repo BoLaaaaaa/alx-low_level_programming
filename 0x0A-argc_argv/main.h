c30bd0c8aeb01e48715cc7abd34c7aa722eaf4de
pola <bolapop40@gmail.com>
2023-07-31 13:51:23 -0700

    c a

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int whatsmyname(void);
+int countargs(int argc);
+void printargs(int argc, char *argv[]);
+int mul(int a, int b);
+int add(int a, int b);
+int change(int cents);
+
+#endif /* MAIN_H */
+
