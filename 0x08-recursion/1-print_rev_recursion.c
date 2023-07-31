3f59f64862cacb218d6c5631f1296558ad4ead0a
pola <bolapop40@gmail.com>
2023-07-31 02:32:54 -0700

    cr

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return; // Base case: Stop recursion when the end of the string is reached
+if (*s == '\0')
+return; 
 
-    _print_rev_recursion(s + 1); // Move to the next character in the string recursively
-    _putchar(*s); // Print the current character after the recursive call (in reverse order)
+_print_rev_recursion(s + 1); 
+_putchar(*s);
 }
 
