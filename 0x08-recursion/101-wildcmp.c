5679ac7fec159441d3aa860375e8ca80d5418d5c
pola <bolapop40@gmail.com>
2023-07-31 13:38:21 -0700

    c r

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,22 +9,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*')
-            return wildcmp(s1, s2 + 1);
-        else if (*(s2 + 1) == '\0')
-            return 1;
-        else if (*s1 == '\0')
-            return 0;
-        else
-            return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-    }
-    else if (*s1 != *s2)
-        return 0;
-    else if (*s1 == '\0')
-        return 1;
-
-    return wildcmp(s1 + 1, s2 + 1);
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return wildcmp(s1, s2 + 1);
+else if (*(s2 + 1) == '\0')
+return (1);
+else if (*s1 == '\0')
+return (0);
+else
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
-
+else if (*s1 != *s2)
+return (0);
+else if (*s1 == '\0')
+return (1);
+return (wildcmp(s1 + 1, s2 + 1));
+}
