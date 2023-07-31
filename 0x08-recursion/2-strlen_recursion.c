ca043e1612100f6131b08bdcec0c65c02aa64d2c
pola <bolapop40@gmail.com>
2023-07-31 02:10:57 -0700

    cr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,12 @@
  * _strlen_recursion - Returns the length of a string using recursion
  * @s: Pointer to the string whose length is to be determined
  *
- * Return: The length of the string
+ * Return: int
  */
 int _strlen_recursion(char *s)
 {
     if (*s == '\0')
-        return 0; // Base case: The length of an empty string is 0
+        return 0; 
 
-    return (1 + _strlen_recursion(s + 1)); // Recursive call with the rest of the string
+    return (1 + _strlen_recursion(s + 1)); 
 }
-
