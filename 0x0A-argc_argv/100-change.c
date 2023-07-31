04c634f90e477f6c6cb383930fcf9c4540388e5d
pola <bolapop40@gmail.com>
2023-07-31 03:41:58 -0700

    c ar

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int minCoins(int cents) {
+int coins[] = {25, 10, 5, 2, 1};
+int numCoins = sizeof(coins) / sizeof(coins[0]);
+int count = 0;
+if (cents < 0)
+return 0;
+for (int i = 0; i < numCoins; i++) {
+while (cents >= coins[i]) {
+cents -= coins[i];
+count++;
+}
+}
+return count;
+}
+int main(int argc, char *argv[]) {
+if (argc != 2) {
+printf("Error\n");
+return 1;
+}
+int cents = atoi(argv[1]);
+int result = minCoins(cents);
+printf("%d\n", result);
+return 0;
+}
