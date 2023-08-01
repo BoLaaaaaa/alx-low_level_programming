0d7fc4722e8d6f3cc3ad2e50e260cb76b8b2fcb7
pola <bolapop40@gmail.com>
2023-08-01 04:21:00 -0700

    c fu

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name using the provided function.
+ * @name: Name of the person.
+ * @f: Pointer to the function to be used to print the name.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
