80f06453163a529b41675de4239081aa6b9c1291
pola <bolapop40@gmail.com>
2023-08-01 03:46:07 -0700

    c st

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 /**
  * struct dog - a dog struct
+ * 
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
@@ -16,4 +17,6 @@ char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* DOG_H */
