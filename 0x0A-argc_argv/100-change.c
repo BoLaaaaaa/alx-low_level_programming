f6d9e1483ee951164f354d46b1a33d43263da100
pola <bolapop40@gmail.com>
2023-09-01 05:00:41 -0700

    c a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,45 +6,27 @@
  * @argv: An array of pointers to the arguments
  * Return: 0 on success, 1 on failure
  */
-int main(int argc, char *argv[])
-{
-int amount, coins = 0;
-if (argc != 2)
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+if (argc != 2) {
 printf("Error\n");
 return (1);
 }
-amount = atoi(argv[1]);
-if (amount < 0)
-{
+int cents = atoi(argv[1]);
+if (cents < 0) {
 printf("0\n");
 return (0);
 }
-while (amount > 0)
-{
-if (amount >= 25)
-{
-coins += amount / 25;
-amount %= 25;
+int coins = 0;
+int values[] = {25, 10, 5, 2, 1};
+for (int i = 0; i < 5; i++) {
+while (cents >= values[i]) {
+cents -= values[i];
+coins++;
 }
-else if (amount >= 10)
-{
-coins += amount / 10;
-amount %= 10;
 }
-else if (amount >= 5)
-{
-coins += amount / 5;
-amount %= 5;
-}
-else if (amount >= 2)
-{
-coins += amount / 2;
-amount %= 2;
-}
-else
-{
-coins += amount;
-amount = 0}}
 printf("%d\n", coins);
-return ((0))};
+return (0);
+}
