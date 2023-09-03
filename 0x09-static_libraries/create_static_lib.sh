588caa457c75cf79989bace0a7347e7c9c6146b9
pola <bolapop40@gmail.com>
2023-09-03 04:17:32 -0700

    c 9

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
 gcc -c *.c
-
-# Create the static library liball.a from the object files
-ar rcs liball.a *.o
-
-# Clean up by removing the object files
-rm -f *.o
+ar rc liball.a *.o
