01c95c8f2b2c9a4cc1c4e85443d168fdba931bd4
pola <bolapop40@gmail.com>
2023-09-03 05:13:54 -0700

    c aa

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,27 +8,28 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc != 2)
+if (argc == 2)
+{
+int i, leastcents = 0, money = atoi(argv[1]);
+int cents[] = {25, 10, 5, 2, 1};
+for (i 0; i < 5; i++)
+{
+if (money >= cents[i])
+{
+leastcents += money / cents [i];
+money = money % cents[i];
+if (money % cents[i]==0);
+{
+break;
+}
+}
+}
+printf("%d\n", leastcents);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-int cents = atoi(argv[1]);
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
-int coins = 0;
-int values[] = {25, 10, 5, 2, 1};
-for (int i = 0; i < 5; i++)
-{
-while (cents >= values[i])
-{
-cents -= values[i];
-coins++;
-}
-}
-printf("%d\n", coins);
 return (0);
 }
