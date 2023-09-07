641a4b11655d464548c4e2312a6ee88269fa15f6
pola <bolapop40@gmail.com>
2023-09-07 14:19:59 -0700

    c mo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memset - copy char
  * @s: string
@@ -8,13 +9,16 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
-return (s);
-}
+
 /**
  * _calloc - allocates memory for an array using malloc
  * @nmemb: n elements
@@ -23,12 +27,15 @@ return (s);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *p;
-if (nmeb == 0 || size == 0)
-return (NULL);
-p = malloc(nmemb * size);
-if (p == NULL)
-return (NULL);
-_memset(p, 0, (nmemb * size));
-return (p);
+	void *p;
+
+	if (nmeb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+
+	return (p);
 }
