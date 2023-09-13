4a173e65b3acd2a05cb8f77886cd0d0c5c05bc25
pola <bolapop40@gmail.com>
2023-09-13 15:12:06 -0700

    c fu

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,42 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0 (on success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-int i, bytes;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-bytes = atoi(argv[1]);
-if (bytes < 0)
-{
-printf("Error\n");
-return (2);
-}
-for (i = 0; i < bytes; i++)
-{
-printf("%02hhx ", ((char *)&main)[i]);
-}
-printf("\n");
-return (0);
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
