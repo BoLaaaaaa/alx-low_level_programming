c2f7e131f0c1ef92321d4e4dd6bbda03c36a9a3e
pola <bolapop40@gmail.com>
2023-09-13 15:18:22 -0700

    c fu

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,29 +1,33 @@
-#include <stddef.h>
 #include "3-calc.h"
-#include <stdlib.h>
+
 /**
- * get_op_func - selects the correct function to perform the operation
- * @s: The operator passed as argument to the program
- * Return:Pointerto the function that corresponds to
- * the operator given as a parameter
- * NULL if theoperatordoes not match any of the 5expectedoperators (+,-,*,/,%)
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
-while (ops[i].op)
-{
-if (*s == *(ops[i].op) && *(s + 1) == '\0')
-return (ops[i].f);
-i++;
-}
-return (NULL);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
